53ea7f51bb23c7b0759d60a00db6f409f247fb13
swahed <serjoscha.wahed@gmail.com>
2020-04-05 12:27:18 +0200

    Imported initial game class with tests

---
+++ game.js
@@ -0,0 +1,92 @@
+// https://www.youtube.com/watch?v=ThwR-8Tc83w
+exports.Game = class game {
+    colors = ["blue", "red", "green", "yellow"];
+    maxValue = 13;
+    cards = [];
+    gamePhases = ["new", "deal", "bid", "play", "results", "complete"];
+    state = {
+        phase: "new",
+        players: [],
+        decks: [],
+        round: 1,
+        currentTrick : [],
+        tricks: [],
+        maxRounds: null,
+        playersTurn : null
+    }
+    constructor() {
+        for (const color of this.colors) {
+            for (let index = 1; index <= this.maxValue; index++) {
+                this.cards.push([color, index]);
+            }
+        }
+    }
+    _drawCards() {
+        const cardStack = [...this.cards];
+        this.state.decks = [];
+        for (let i = 0; i <= this.state.players.length - 1; i++) {
+            let deck = [];
+            this.state.decks.push(deck);
+            for (let j = 0; j < this.state.round; j++) {
+                let random = Math.floor(Math.random() * cardStack.length);
+                let card = cardStack[random];
+                deck.push(card);
+                cardStack.splice(random, 1);
+            }
+        }
+    }
+    start() {
+        if (this.state.players.length < 3) throw new Error("Cannot start without minimum of 3 players.");
+        if (this.state.players.length > 6) throw new Error("Cannot start with more than 6 players.");
+        if (this.state.phase != this.gamePhases[0]) throw new Error( "Game is already in progress.");
+        this.state.phase = this.gamePhases[1];
+        this.state.playersTurn = 1;
+        this._drawCards();
+    }
+    addPlayer(name) {
+        if (this.state.phase != "new") throw new Error("Cannot add new player.");
+        if (!name) throw new Error("New Player cannot be added without a name.");
+
+        this.state.players.push(name);
+        this.state.maxRounds = ~~(this.cards.length / this.state.players.length);
+        return this.state.players.length;
+    }
+    getCards(playerId) {
+        return this.state.decks[playerId - 1];
+    }
+    playCard(playerId, cardIndex){
+        if (playerId != this.state.playersTurn) throw new Error(`Wrong player, please wait for turn of player ${this.state.playersTurn}.`);
+        this.state.playersTurn++;
+        if (this.state.playersTurn > this.state.players.length){
+            let firstColorInTrick = this.state.currentTrick[0][0];
+            var winnerCard = this.state.currentTrick.filter(c => c[0] === firstColorInTrick).reduce((prev, curr) => {
+                if(!prev || curr[1] > prev[1]) return curr;
+                else return prev;
+            });
+            let winner = this.state.currentTrick.indexOf(winnerCard) + 1;
+            this.state.tricks.push(winner);
+            this.state.playersTurn = 0;
+        }
+        let deck = this.state.decks[playerId -1];
+        let card = deck[cardIndex];
+        let cardColor = card[0];
+        if(this.state.currentTrick.length > 0) {
+            let firstColorInTrick = this.state.currentTrick[0][0];
+            if (firstColorInTrick !== cardColor &&
+                deck.filter(c => c[0] === firstColorInTrick).length > 0) {
+                throw new Error("Wrong color, please play color red.");
+            }
+        }
+        this.state.currentTrick.push(card);
+        return card;
+    }
+    finishRound() {
+        if (this.state.round < this.state.maxRounds) {
+            this.state.round++;
+            this._drawCards();
+        }
+        else {
+            this.state.phase = this.gamePhases[this.gamePhases.length - 1]
+        };
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,396 @@
+var assert = require('assert');
+var game = new require('../game').Game;
+
+describe('game setup', function () {
+    let testGame;
+    beforeEach(function () {
+        testGame = new game();
+    });
+
+    it('should be defined', function () {
+        assert.notEqual(game, undefined);
+    });
+
+    it('should be instatable', function () {
+        assert.notEqual(testGame, undefined);
+    });
+
+    it('should know the 4 colors', function () {
+        assert.deepEqual(testGame.colors.sort(), ["blue", "green", "red", "yellow"].sort());
+    });
+
+    it('should know the max value is 13', function () {
+        for (let index = 0; index <= 13; index++) {
+            assert.deepEqual(testGame.maxValue, 13);
+        }
+    });
+
+    it('should have cards for each color and value', function () {
+        const cards = [["blue", 1], ["blue", 2], ["blue", 3], ["blue", 4], ["blue", 5], ["blue", 6], ["blue", 7], ["blue", 8], ["blue", 9], ["blue", 10], ["blue", 11], ["blue", 12], ["blue", 13],
+        ["red", 1], ["red", 2], ["red", 3], ["red", 4], ["red", 5], ["red", 6], ["red", 7], ["red", 8], ["red", 9], ["red", 10], ["red", 11], ["red", 12], ["red", 13],
+        ["green", 1], ["green", 2], ["green", 3], ["green", 4], ["green", 5], ["green", 6], ["green", 7], ["green", 8], ["green", 9], ["green", 10], ["green", 11], ["green", 12], ["green", 13],
+        ["yellow", 1], ["yellow", 2], ["yellow", 3], ["yellow", 4], ["yellow", 5], ["yellow", 6], ["yellow", 7], ["yellow", 8], ["yellow", 9], ["yellow", 10], ["yellow", 11], ["yellow", 12], ["yellow", 13]];
+
+        assert.deepEqual(testGame.cards, cards);
+    });
+
+    it('should know have certain game phases', function () {
+        assert.deepEqual(testGame.gamePhases, ["new", "deal", "bid", "play", "results", "complete"]);
+    });
+
+    it('should have the initial game phases of new', function () {
+        assert.equal(testGame.state.phase, "new");
+    });
+
+    it('should allow multiple players to join', function () {
+        testGame.addPlayer("John");
+        assert.deepEqual(testGame.state.players, ["John"]);
+        testGame.addPlayer("Maria");
+        assert.deepEqual(testGame.state.players, ["John", "Maria"]);
+        testGame.addPlayer("Bert");
+        assert.deepEqual(testGame.state.players, ["John", "Maria", "Bert"]);
+    });
+
+    it('should start with round 1', function () {
+        assert.equal(testGame.state.round, 1);
+    });
+
+    it('should allow for max no of rounds considering player no', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        assert.equal(testGame.state.maxRounds, 26);
+        testGame.addPlayer("Bert");
+        assert.equal(testGame.state.maxRounds, 17);
+    });
+
+    it('should have the initial game phase new', function () {
+        assert.deepEqual(testGame.state.phase, "new");
+    });
+
+    it('should have the phase deal< after the game is started', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("Bert");
+        testGame.start();
+        assert.deepEqual(testGame.state.phase, "deal");
+    });
+
+    it('should accept new player in first stage', function () {
+        assert.doesNotThrow(
+            () => {
+                testGame.addPlayer("John");
+            }
+        );
+    });
+
+    it('should not accept new player the other stages', function () {
+        for (let i = 1; i < testGame.gamePhases.length; i++) {
+            testGame.state.phase = testGame.gamePhases[i];
+
+            assert.throws(
+                () => {
+                    testGame.addPlayer("John");
+                },
+                { message: "Cannot add new player." },
+                `Phase '${testGame.state.phase}'`
+            );
+        }
+    });
+
+    it('should not allow to start < 3 players', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        assert.throws(
+            () => {
+                testGame.start();
+            },
+            { message: "Cannot start without minimum of 3 players." }
+        );
+    });
+
+    it('should not allow to start >= 3 players', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("Bert");
+        assert.doesNotThrow(
+            () => {
+                testGame.start();
+            }
+        );
+    });
+
+    it('should not allow to start > 6 players', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("John");
+        assert.throws(
+            () => {
+                testGame.start();
+            },
+            { message: "Cannot start with more than 6 players." }
+        );
+    });
+
+    it('should not allow to start after game has already been started', function () {
+        testGame.addPlayer("John");
+        testGame.addPlayer("Maria");
+        testGame.addPlayer("John");
+        testGame.start();
+        assert.throws(
+            () => {
+                testGame.start();
+            },
+            { message: "Game is already in progress." }
+        );
+    });
+
+    it('should enforce players to select a name', function () {
+        assert.throws(
+            () => {
+                let id = testGame.addPlayer();
+            },
+            { message: "New Player cannot be added without a name." }
+        );
+    });
+
+    it('should give new players an id', function () {
+        let id = testGame.addPlayer("John");
+        assert(id);
+    });
+
+    it('should give new players a unique id', function () {
+        let id1 = testGame.addPlayer("John");
+        let id2 = testGame.addPlayer("Maria");
+        assert.notEqual(id1, id2);
+    });
+
+});
+
+describe('starting and finishing a round in play', function () {
+    let testGame;
+    let playerId1;
+    let playerId2;
+    let playerId3;
+    let playerId4;
+
+    beforeEach(function () {
+        testGame = new game();
+        playerId1 = testGame.addPlayer("John");
+        playerId2 = testGame.addPlayer("Maria");
+        playerId3 = testGame.addPlayer("Bert");
+        playerId4 = testGame.addPlayer("Ann");
+        testGame.start();
+    });
+
+    it('should draw cards for each player', function () {
+        assert.equal(testGame.state.decks.length, testGame.state.players.length);
+        testGame.finishRound();
+        assert.equal(testGame.state.decks.length, testGame.state.players.length);
+    });
+
+    it('should draw as many cards for each players as are available for the current round', function () {
+        let cards = testGame.getCards(playerId1);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId2);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId3);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId4);
+        assert.equal(testGame.state.round, cards.length);
+
+        testGame.finishRound();
+        cards = testGame.getCards(playerId1);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId2);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId3);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId4);
+        assert.equal(testGame.state.round, cards.length);
+
+        testGame.finishRound();
+        cards = testGame.getCards(playerId1);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId2);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId3);
+        assert.equal(testGame.state.round, cards.length);
+        cards = testGame.getCards(playerId4);
+        assert.equal(testGame.state.round, cards.length);
+    });
+
+    it('should draw different cards for each player', function () {
+        let cards1 = testGame.getCards(playerId1);
+        let cards2 = testGame.getCards(playerId2);
+        let cards3 = testGame.getCards(playerId3);
+        let cards4 = testGame.getCards(playerId4);
+        assert.notDeepEqual(cards1, cards2);
+        assert.notDeepEqual(cards2, cards3);
+        assert.notDeepEqual(cards3, cards4);
+    });
+
+    it('should progress to next round when round is finished', function () {
+        testGame.finishRound();
+        assert.equal(testGame.state.round, 2);
+        testGame.finishRound();
+        testGame.finishRound();
+        testGame.finishRound();
+        assert.equal(testGame.state.round, 5);
+    });
+
+    it('should switch to state to complete after last round', function () {
+        for (let i = 1; i < testGame.state.maxRounds; i++) {
+            testGame.finishRound();
+            assert.equal(testGame.state.phase, "deal");
+        }
+        testGame.finishRound();
+        assert.equal(testGame.state.phase, "complete");
+    });
+});
+
+// https://www.youtube.com/watch?v=VpaV4VVMGE4
+describe('playing cards', function () {
+    let testGame;
+    let playerId1;
+    let playerId2;
+    let playerId3;
+    let playerId4;
+
+    beforeEach(function () {
+        testGame = new game();
+        playerId1 = testGame.addPlayer("John");
+        playerId2 = testGame.addPlayer("Maria");
+        playerId3 = testGame.addPlayer("Bert");
+        playerId4 = testGame.addPlayer("Ann");
+        testGame.start();
+        testGame.finishRound();
+        testGame.finishRound();
+        testGame.state.decks = [
+            [['red', 4], ['yellow', 4], ['green', 7]],
+            [['red', 9], ['green', 2], ['blue', 8]],
+            [['blue', 5], ['yellow', 3], ['green', 13]],
+            [['blue', 6], ['yellow', 14], ['green', 10]]
+        ];
+    });
+
+    it('should be first players turn first', function () {
+        assert.equal(testGame.state.playersTurn, playerId1);
+    });
+
+    it('should start a round with first trick', function () {
+        assert.equal(testGame.state.tricks.length, 0);
+    });
+
+    it('should be possible for player 1 to play a card first', function () {
+        assert.doesNotThrow(
+            () => {
+                let card = testGame.playCard(playerId1, 0);
+                assert.deepEqual(card, ['red', 4]);
+            }
+        );
+    });
+
+    it('should not be possible for player 2, 3, 4 to play a card first', function () {
+        assert.throws(
+            () => {
+                testGame.playCard(playerId2, 0);
+            },
+            { message: "Wrong player, please wait for turn of player 1." }
+        );
+        assert.throws(
+            () => {
+                testGame.playCard(playerId3, 0);
+            },
+            { message: "Wrong player, please wait for turn of player 1." }
+        );
+        assert.throws(
+            () => {
+                testGame.playCard(playerId4, 0);
+            },
+            { message: "Wrong player, please wait for turn of player 1." }
+        );
+    });
+
+    it('should be possible for player 2, 3, 4 to play card succinctly', function () {
+        testGame.playCard(playerId1, 0);
+        assert.doesNotThrow(
+            () => {
+                let card = testGame.playCard(playerId2, 0);
+                assert.deepEqual(card, ['red', 9]);
+            }
+        );
+        assert.doesNotThrow(
+            () => {
+                let card = testGame.playCard(playerId3, 0);
+                assert.deepEqual(card, ['blue', 5]);
+            }
+        );
+        assert.doesNotThrow(
+            () => {
+                let card = testGame.playCard(playerId4, 0);
+                assert.deepEqual(card, ['blue', 6]);
+            }
+        );
+    });
+
+    it('should be possible to follow a card in the same color', function () {
+        let card1 = testGame.playCard(playerId1, 0); // ['red', 4]
+        let card2;
+        assert.doesNotThrow(
+            () => {
+                card2 = testGame.playCard(playerId2, 0); // ['red', 9]
+            }
+        );
+        assert.equal(card1[0], card2[0]);
+    });
+
+    it('should not be possible to follow a card in another color, if one has color on hand.', function () {
+        let card1 = testGame.playCard(playerId1, 0); // ['red', 4]
+        let card2;
+        assert.throws(
+            () => {
+                card2 = testGame.playCard(playerId2, 1); // ['green', 2]
+            },
+            { message: "Wrong color, please play color red." }
+        );
+    });
+
+    it('should be possible to follow a card in another color, if one does not hava color on hand.', function () {
+        let card1 = testGame.playCard(playerId1, 0); // ['red', 4]
+        let card2 = testGame.playCard(playerId2, 0); // ['red', 9]
+        assert.doesNotThrow(
+            () => {
+                card2 = testGame.playCard(playerId3, 0); // ['blue', 5]
+            }
+        );
+    });
+
+    it('should be the second trick after all players have placed their cards', function () {
+        testGame.playCard(playerId1, 0);
+        testGame.playCard(playerId2, 0);
+        testGame.playCard(playerId3, 0);
+        testGame.playCard(playerId4, 0);
+        assert.equal(testGame.state.tricks.length, 1);
+    });
+
+    it('should declare the highest card in the trump color the winner of the trick', function () {
+        testGame.playCard(playerId1, 0); // ['red', 4]
+        testGame.playCard(playerId2, 0); // ['red', 9]
+        testGame.playCard(playerId3, 0); // ['blue', 5]
+        testGame.playCard(playerId4, 0); // ['blue', 7]
+        assert.equal(testGame.state.tricks[0], playerId2);
+    });
+
+    // cannot pla same card twice
+    // should be the first players turn again after last player trick
+
+
+
+    // it('should be eachs players turn one after the other', function () {
+    //     assert.equal(testGame.state.playersTurn, playerId1);
+    // });
+});
